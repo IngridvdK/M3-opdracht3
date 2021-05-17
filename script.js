c261d3e51abae36fa3ed6604f04d0600dc786dcb
IngridvdK <79993280+IngridvdK@users.noreply.github.com>
2021-05-17 09:28:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const age = 20;
+if (age > 18) {
+    console.log("Kom gezellig binnen");
+}
+    else if (age == 18) {
+    console.log("Je bent van harte welkom");
+    }
+        else {
+    console.log("Helaas, jij mag hier nog niet naar binnen");
+        }
+
+const isFemale = true;
+if (isFemale == true) {
+    console.log("Alsjeblieft je kaartje voor de Ladiesnight");
+}
+    else {
+    console.log("Helaas, deze kaarten zijn niet beschikbaar voor jou");
+    }
+
+const driverSatus = 'bob';
+if (driverSatus == 'bob') {
+    console.log("Je mag naar huis rijden, fijne avond nog");
+}
+    else {
+    console.log("We bellen een taxi voor je zodat je veilig thuis komt, even geduld")
+    }
